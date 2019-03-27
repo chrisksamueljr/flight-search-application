d3773fc19cff088ab3e3f9e9cab751f5273a97aa
Chris Samuel <ksamuel.chris@gmail.com>
2019-03-27 17:40:59 -0400

    generated and added styling

---
+++ src/app/flight-detail/history-navigation/history-navigation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'history-navigation',
+  templateUrl: './history-navigation.component.html',
+  styleUrls: ['./history-navigation.component.scss']
+})
+export class HistoryNavigationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
